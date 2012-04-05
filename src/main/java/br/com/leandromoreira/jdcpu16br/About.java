5a6344a888997c06c8e8eea3a38d8e359a9dcd3d
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-04-05 18:18:34 -0300

    putting final to real constants

--- src/main/java/br/com/leandromoreira/jdcpu16br/About.java
+++ src/main/java/br/com/leandromoreira/jdcpu16br/About.java
@@ -1,9 +1,9 @@
 package br.com.leandromoreira.jdcpu16br;
 
 public class About {
-    public static String VM_NAME = "DCPU-16";
-    public static String VM_VERSION = "1.1";
-    public static String VM_SPEC = "http://0x10c.com/doc/dcpu-16.txt";
-    public static String COPYRIGHT = "2012 Mojang";
+    public static final String VM_NAME = "DCPU-16";
+    public static final String VM_VERSION = "1.1";
+    public static final String VM_SPEC = "http://0x10c.com/doc/dcpu-16.txt";
+    public static final String COPYRIGHT = "2012 Mojang";
     
 }
