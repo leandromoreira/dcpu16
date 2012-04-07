5b68dc59ce75335a07a8b54b58a3b8b26c5ae427
Leandro Moreira <leandro.rhcp99@gmail.com>
2012-04-07 19:21:35 -0300

    started the debugger window (now its only mocked jframe) made memory load and its tests defined the entry point of app

--- src/main/java/br/com/leandromoreira/jdcpu16br/Memory.java
+++ src/main/java/br/com/leandromoreira/jdcpu16br/Memory.java
@@ -4,6 +4,7 @@ public class Memory {
 
     private static final int MEMORY_SIZE = 0x10000;
     private final int[] memory = new int[MEMORY_SIZE];
+    private int maximumFilled = 0;
 
     public int readFrom(final int address) {
         return memory[address];
@@ -11,5 +12,30 @@ public class Memory {
 
     public void writeAt(final int address, final int value) {
         memory[address] = value;
+    }
+
+    private void clear() {
+        if (!isEmpty()) {
+            for (int address = 0x0000; address < MEMORY_SIZE; address++) {
+                memory[address] = 0x0000;
+            }
+        }
+    }
+
+    public boolean isEmpty() {
+        return maximumFilled == 0;
+    }
+
+    public void load(String[] hexadecimalCells) {
+        if (hexadecimalCells != null) {
+            clear();
+            int address = 0x0000;
+            for (final String oneValue : hexadecimalCells) {
+                if (oneValue != null && !"".equals(oneValue.trim())) {
+                    memory[address++] = Integer.valueOf(oneValue, 16);
+                    ++maximumFilled;
+                }
+            }
+        }
     }
 }
--- src/test/java/br/com/leandromoreira/jdcpu16br/MemoryTest.java
+++ src/test/java/br/com/leandromoreira/jdcpu16br/MemoryTest.java
@@ -2,14 +2,51 @@ package br.com.leandromoreira.jdcpu16br;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MemoryTest {
 
+    private Memory memory;
+
+    @Before
+    public void setup() {
+        memory = new Memory();
+    }
+
     @Test
     public void it_handles_io() {
-        final Memory memory = new Memory();
         memory.writeAt(0x0000, 0x7C01);
         assertThat(memory.readFrom(0x0000), is(0x7C01));
+    }
+
+    @Test
+    public void it_is_empty_when_nothing_is_loaded() {
+        assertThat(memory.isEmpty(), is(true));
+    }
+
+    @Test
+    public void it_load_memory_from_hexa_string_array() {
+        final String[] cells = new String[]{"003A"};
+        memory.load(cells);
+        assertThat(memory.isEmpty(), is(false));
+        assertThat(memory.readFrom(0x0000), is(0x003A));
+    }
+
+    @Test
+    public void it_null_empty_array() {
+        final String[] emptyCell = new String[]{};
+        final String[] nullCell = null;
+        memory.load(emptyCell);
+        assertThat(memory.isEmpty(), is(true));
+        memory.load(nullCell);
+        assertThat(memory.isEmpty(), is(true));
+    }
+
+    @Test
+    public void it_ignores_empty_or_null_item_of_array() {
+        final String[] emptyCell = new String[]{null, ""};
+        memory.load(emptyCell);
+        assertThat(memory.isEmpty(), is(true));
     }
 }
